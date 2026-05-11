589693bd32cb5060992b8a80a99f42c240455272
Ayush Kumar <ayush.2007.iit@gmail.com>
2026-05-11 23:24:52 +0530

    feat: db connection and table migrations (#9)
    * feat: added a getenv helper
    
    * feat: added postgres connection using gorm
    
    * feat: added db migrations

---
+++ config/db.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ayush00git/cms-web/helpers"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	DB_USER := helpers.GetEnv("DB_USER")
+	DB_NAME := helpers.GetEnv("DB_NAME")
+	DB_PASS := helpers.GetEnv("DB_PASS")
+	// DB_PORT := helpers.GetEnv("DB_PORT") // will see during deployment
+
+	dsn := fmt.Sprintf("host=localhost user=%s password=%s dbname=%s port=5432 sslmode=disable", DB_USER, DB_PASS, DB_NAME)
+	
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to the database")
+	}
+
+	DB = db
+	log.Println("Database connected")
+}
