9c5aeedfba131832151f35b9ac18616656ddf15f
ayush00git <ayush.2007.iit@gmail.com>
2026-05-13 18:04:36 +0530

    pkg: defined auth handlers

--- handlers/auth.go
+++ handlers/auth.go
@@ -38,7 +38,7 @@ func (h *AuthHandler) FacultySignup (c *gin.Context) {
 	// log.Printf("Error imma lookin for: %s", result.Error)
 	if result.Error != nil {
 		if strings.Contains(result.Error.Error(), "unique constraint") {
-			c.JSON(409, gin.H{"error": "user with that creds already exists, login instead"})
+			c.JSON(409, gin.H{"error": "user with that email already exists! login instead"})
 			return
 		}
 		c.JSON(500, gin.H{"error": "failed inserting object to the table"})
@@ -60,7 +60,7 @@ func (h *AuthHandler) FacultyLogin (c *gin.Context) {
 	var faculty models.Faculty
 	result := h.DB.Where("email = ?", inputs.Email).Take(&faculty)
 	if result.Error != nil {
-		c.JSON(401, gin.H{"error": "user with that email doesn't exists"})
+		c.JSON(404, gin.H{"error": "user with that email doesn't exists"})
 		return
 	}
 
@@ -88,4 +88,140 @@ func (h *AuthHandler) FacultyLogin (c *gin.Context) {
 	)
 
 	c.JSON(200, gin.H{"success": "logged in successfully"})
+}
+
+func (h *AuthHandler) WardenSignup (c *gin.Context) {
+	var inputs models.Warden
+
+	if err := c.ShouldBindJSON(&inputs); err != nil {
+		c.JSON(400, gin.H{"error": "request body unacceptable"})
+		return
+	}
+
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(inputs.Password), 10)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "unable to hash the password"})
+		return
+	}
+	inputs.Password = string(hashedPass)
+
+	// send a email for account verification
+
+	result := h.DB.Create(&inputs)
+	if result.Error != nil {
+		if strings.Contains(result.Error.Error(), "unique constraint") {
+			c.JSON(409, gin.H{"error": "user with that email already exists! login instead"})
+			return
+		}
+		c.JSON(500, gin.H{"error": "failed inserting object to the table"})
+		return
+	}
+	c.JSON(201, gin.H{"success": "signup success!"})	
+}
+
+func (h *AuthHandler) WardenLogin (c *gin.Context) {
+	var inputs models.Warden
+
+	if err := c.ShouldBindJSON(&inputs); err != nil {
+		c.JSON(400, gin.H{"error": "request body unacceptable"})
+		return
+	}
+
+	var warden models.Warden
+	result := h.DB.Where("email = ?", inputs.Email).Take(&warden)
+	if result.Error != nil {
+		c.JSON(404, gin.H{"error": "user with that email doesn't exists"})
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(warden.Password), []byte(inputs.Password))
+	if err != nil {
+		c.JSON(401, gin.H{"error": "incorrect password"})
+		return
+	}
+
+	token, err := helpers.GenerateToken(inputs.Email)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "unable to sign the jwt token"})
+		return
+	}
+
+	c.SetCookie(
+		"token",
+		token,
+		30 * 24 * 60 * 60 * 1000,
+		"/",
+		"localhost",
+		false,
+		true,
+	)
+
+	c.JSON(200, gin.H{"success": "logged in successfully!"})
+}
+
+func (h *AuthHandler) CentreHeadSignup (c *gin.Context) {
+	var inputs models.CentreHead
+
+	if err := c.ShouldBindJSON(&inputs); err != nil {
+		c.JSON(400, gin.H{"error": "request body unacceptable"})
+		return
+	}
+
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(inputs.Password), 10)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "unable to hash the password"})
+		return
+	}
+
+	inputs.Password = string(hashedPass)
+	
+	result := h.DB.Create(&inputs)
+	if result.Error != nil {
+		if strings.Contains(result.Error.Error(), "unique constraint") {
+			c.JSON(409, gin.H{"error": "user with that email already exists, login instead"})
+			return
+		}
+	}
+
+	c.JSON(201, gin.H{"success": "signup success!"})
+}
+
+func (h *AuthHandler) CentreHeadLogin (c *gin.Context) {
+	var inputs models.CentreHead
+
+	if err := c.ShouldBindJSON(&inputs); err != nil {
+		c.JSON(400, gin.H{"error": "error body unacceptable"})
+		return
+	}
+
+	var head models.CentreHead
+	result := h.DB.Where("email = ?", inputs.Email).Take(&head)
+	if result.Error != nil {
+		c.JSON(404, gin.H{"error": "user with that email doesn't exists"})
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(head.Password), []byte(inputs.Password))
+	if err != nil {
+		c.JSON(500, gin.H{"error": "incorrect password"})
+		return
+	}
+
+	token, err := helpers.GenerateToken(inputs.Email)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "unable to sign the jwt token"})
+		return
+	}
+
+	c.SetCookie(
+		"token",
+		token,
+		30 * 24 * 60 * 60 * 1000,
+		"/",
+		"localhost",
+		false,
+		true,
+	)
+	
+	c.JSON(200, gin.H{"success": "logged in successfully!"})
 }
