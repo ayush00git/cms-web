62157baa462f7c8fd8799f0d107c389027109f7c
ayush00git <ayush.2007.iit@gmail.com>
2026-05-14 00:26:05 +0530

    pkg: admin login api added

--- models/admin_auth.go
+++ models/admin_auth.go
@@ -17,7 +17,13 @@ const (
 type Admin struct {
 	ID				uint			`gorm:"primaryKey;autoIncrement" json:"id"`
 	Email			string			`gorm:"uniqueIndex;not null" json:"email"`
+	Password		string			`gorm:"not null" json:"password"`
 	Post			PostType		`gorm:"type:varchar(15);unique;not null" json:"post"`
 	IsVerified		bool			`gorm:"default:false" json:"is_verified"`
 	CreatedAt		time.Time		`json:"created_at"`
+}
+
+type AdminLogin struct {
+	Email			string			`json:"email" binding:"required"`
+	Password		string			`json:"password" binding:"required"`
 }
