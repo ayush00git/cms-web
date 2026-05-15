ee64dba1545f6ad0d8aacdc26b3cd3aaa64a80a5
ayush00git <ayush.2007.iit@gmail.com>
2026-05-15 20:05:17 +0530

    fix: applied middleware to routes

--- routes/complaint.go
+++ routes/complaint.go
@@ -2,15 +2,16 @@ package routes
 
 import (
 	"github.com/ayush00git/cms-web/handlers"
+	"github.com/ayush00git/cms-web/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ComplaintRoute(e *gin.Engine, h *handlers.ComplaintHandler) {
 	
-	e.POST("/api/complaint/faculty", h.FacultyReportComplaint)
-	e.POST("/api/complaint/warden", h.WardenReportComplaint)
-	e.POST("/api/complaint/centrehead", h.CentreHeadReportComplaint)
+	e.POST("/api/complaint/faculty", middleware.IsAuthenticated(), h.FacultyComplaint)
+	e.POST("/api/complaint/warden", middleware.IsAuthenticated(), h.WardenComplaint)
+	e.POST("/api/complaint/centre_head", middleware.IsAuthenticated(), h.CentreHeadComplaint)
 
 	admin := e.Group("/api/complaint/admin")
 	{
