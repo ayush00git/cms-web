3c7c24d8e7aa424f5f427d23ecb4518e919b7dfa
ayush00git <ayush.2007.iit@gmail.com>
2026-05-16 14:05:13 +0530

    fix: set to users api only

--- routes/complaint.go
+++ routes/complaint.go
@@ -8,18 +8,7 @@ import (
 )
 
 func ComplaintRoute(e *gin.Engine, h *handlers.ComplaintHandler) {
-	
 	e.POST("/api/complaint/faculty", middleware.IsAuthenticated(), h.FacultyComplaint)
 	e.POST("/api/complaint/warden", middleware.IsAuthenticated(), h.WardenComplaint)
 	e.POST("/api/complaint/centre_head", middleware.IsAuthenticated(), h.CentreHeadComplaint)
-
-	admin := e.Group("/api/complaint/admin")
-	{
-		admin.POST("/xen/status", h.XENUpdateStatus)
-		admin.POST("/ae/status", h.AEUpdateStatus)
-		admin.POST("/je/status", h.JEUpdateStatus)
-	}
-
-	// Public dashboard
-	e.GET("api/complaints/public", h.GetPublicDashboard)
 }
