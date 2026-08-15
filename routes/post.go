ec1cd68902f707d9fa5f71a4c25882c5579311f6
ayush00git <ayush.2007.iit@gmail.com>
2026-08-16 02:15:18 +0530

    fix: add a group custom route rate limiter

--- routes/post.go
+++ routes/post.go
@@ -8,29 +8,36 @@ import (
 )
 
 func PostRoute(e *gin.Engine, h *handlers.PostHandler) {
-	// APIs for new post
-	e.POST("/api/posts/faculty", middleware.IsAuthenticated(), h.FacultyPost)
-	e.POST("/api/posts/warden", middleware.IsAuthenticated(), h.WardenPost)
-	e.POST("/api/posts/centrehead", middleware.IsAuthenticated(), h.CentreheadPost)
 
-	// APIs for updating the post
-	e.PATCH("/api/posts/faculty/edit/:post_id", middleware.IsAuthenticated(), h.FacultyPostEdit)
-	e.PATCH("/api/posts/warden/edit/:post_id", middleware.IsAuthenticated(), h.WardenPostEdit)
-	e.PATCH("/api/posts/centrehead/edit/:post_id", middleware.IsAuthenticated(), h.CentreheadPostEdit)
+	postRoutesRateLimiter := middleware.NewRateLimiter(200, 1.0/10.0)
 
-	// APIs for deleting the post
-	e.DELETE("/api/posts/faculty/delete/:post_id", middleware.IsAuthenticated(), h.FacultyPostDelete)
-	e.DELETE("/api/posts/warden/delete/:post_id", middleware.IsAuthenticated(), h.WardenPostDelete)
-	e.DELETE("/api/posts/centrehead/delete/:post_id", middleware.IsAuthenticated(), h.CentreheadPostDelete)
+	posts := e.Group("/api/posts", middleware.IsAuthenticated(), postRoutesRateLimiter.LimitByAuth())
+	{
+		// APIs for new post
+		posts.POST("/faculty", h.FacultyPost)
+		posts.POST("/warden", h.WardenPost)
+		posts.POST("/centrehead", h.CentreheadPost)
 
-	// APIs for getting the posts
-	e.GET("/api/posts/faculty", middleware.IsAuthenticated(), h.GetFacultyPosts)
-	e.GET("/api/posts/warden", middleware.IsAuthenticated(), h.GetWardenPosts)
-	e.GET("/api/posts/centrehead", middleware.IsAuthenticated(), h.GetCentreheadPosts)
-	e.GET("/api/posts/:role/:post_id", middleware.IsAuthenticated(), h.GetPostByID)
+		// APIs for updating the post
+		posts.PATCH("/faculty/edit/:post_id", h.FacultyPostEdit)
+		posts.PATCH("/warden/edit/:post_id", h.WardenPostEdit)
+		posts.PATCH("/centrehead/edit/:post_id", h.CentreheadPostEdit)
 
-	// APIs for comments on the posts
-	e.POST("/api/posts/faculty/comment/:post_id", middleware.IsAuthenticated() ,h.FacultyPostComment)
-	e.POST("/api/posts/warden/comment/:post_id", middleware.IsAuthenticated(), h.WardenPostComment)
-	e.POST("/api/posts/centrehead/comment/:post_id", middleware.IsAuthenticated(), h.CentreheadPostComment)
+		// APIs for deleting the post
+		posts.DELETE("/faculty/delete/:post_id", h.FacultyPostDelete)
+		posts.DELETE("/warden/delete/:post_id", h.WardenPostDelete)
+		posts.DELETE("/centrehead/delete/:post_id", h.CentreheadPostDelete)
+
+		// APIs for getting the posts
+		posts.GET("/faculty", h.GetFacultyPosts)
+		posts.GET("/warden", h.GetWardenPosts)
+		posts.GET("/centrehead", h.GetCentreheadPosts)
+		posts.GET("/:role/:post_id", h.GetPostByID)
+
+		// APIs for comments on the posts
+		posts.POST("/faculty/comment/:post_id", h.FacultyPostComment)
+		posts.POST("/warden/comment/:post_id", h.WardenPostComment)
+		posts.POST("/centrehead/comment/:post_id", h.CentreheadPostComment)
+	}
+
 }
