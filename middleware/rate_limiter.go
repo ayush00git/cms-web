6c68cf116ce98f4cb7d99eb3e56dd78bcd3a24bb
ayush00git <ayush.2007.iit@gmail.com>
2026-08-16 01:21:55 +0530

    fix: keep the public/auth rate logic modular

--- middleware/rate_limiter.go
+++ middleware/rate_limiter.go
@@ -3,8 +3,6 @@ package middleware
 import (
 	"sync"
 	"time"
-
-	"github.com/gin-gonic/gin"
 )
 
 type TokenBucket struct {
@@ -90,24 +88,4 @@ func (b *TokenBucket) Allow() bool {
 
 	// otherwise block the request (if token < 1)
 	return false
-}
-
-// Limit is the middleware which looks for bucket associated with the IP / assigns a new one.
-// And returns 429 if rate limit exceeded.
-func (rl *RateLimiter) Limit() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		ip := c.ClientIP()
-		bucket := rl.GetBucket(ip)
-
-		rl.mu.Lock()
-		allowed := bucket.Allow()
-		rl.mu.Unlock()
-
-		if !allowed {
-			c.JSON(429, gin.H{"error": "you've hit it too many times! get back later"})
-			c.Abort()
-			return
-		}
-		c.Next()
-	}
 }
