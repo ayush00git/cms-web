5f9663e127c75324a51b118cba43ba739ba63b2c
ayush00git <ayush.2007.iit@gmail.com>
2026-09-11 15:23:00 +0530

    feat: register a assign superadmin route

--- models/superadmin.go
+++ models/superadmin.go
@@ -7,7 +7,7 @@ import (
 type SuperAdmin struct {
 	ID              uint            `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name			string			`gorm:"not null" json:"name" binding:"required,max=50"`
-	Email			string			`gorm:"not null" json:"email" binding:"required,max=255"`
+	Email			string			`gorm:"uniqueIndex;not null" json:"email" binding:"required,email,max=255"`
 	VisitedAt		time.Time		`json:"visited_at"`
 	CreatedAt		time.Time		`json:"created_at"`
 }
