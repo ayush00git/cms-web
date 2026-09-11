c676d5814020468540e3609094a7a417a8d95c37
ayush00git <ayush.2007.iit@gmail.com>
2026-09-11 15:38:18 +0530

    fix: send invitation email service herlper added

--- services/superadmin_email.go
+++ services/superadmin_email.go
@@ -34,3 +34,27 @@ func SendProfileAccessMailToSuperAdmins(adminID uint, email string) error {
 	log.Printf("super admin access mail was sent to %s", email)
 	return nil
 }
+
+// SendAccessMailToAssignedSuperAdmins mails a invited user to be a superadmin.
+// (by default the invited one is set to superadmin, acceptance doesn't matters.)
+func SendAccessMailToAssignedSuperAdmins(email string) error {
+	// create the access url
+	frontendURL := helpers.GetEnvWithDefault("FRONTEND_URL", "http://localhost:5173")
+	accessURL := fmt.Sprintf(`%s/superadmin/login`, frontendURL)
+
+	mail := buildEmailHTML(
+		"cms: you have been invited to be a cms superadmin.",
+		"Someone invited you to be a superadmin.",
+		"Accept Invitation",
+		accessURL,
+	)
+
+	// sends the email
+	err := SendMail(email, "Invitation to be a cms superadmin.", mail)
+	if err != nil {
+		return err
+	}
+	log.Printf("new super admin invitation mail was sent to %s", email)
+	return nil
+}
+
